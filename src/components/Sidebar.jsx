e447ba3aeea2a99d8d7fa1412cf87ec6bac3dab6
VenkataNaveenKumarNuthalapati <venkatanaveenkumarnuthalapati@gmail.com>
2024-12-13 07:25:38 +0530

    sidebar modified

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,31 +9,33 @@ const Sidebar = () => {
                 !isMenuOpen
                     ? "translate-x-0 "
                     : "-translate-x-[85px] md:translate-x-0"
-            } bg-white px-1 md:w-[70px] flex-col items-center text-center md:pt-4 absolute md:relative transform transition-all`}
+            } bg-white pr-2 md:w-[70px] flex-col items-center text-center md:pt-4 absolute md:relative transform transition-all`}
         >
-            <div className="hover:bg-gray-100 p-4 rounded-lg ml-1">
+            <div className="hover:bg-gray-100 p-1 text-center rounded-lg ml-1">
                 <i className="fa-solid fa-house text-[17px] mx-2 "></i>
-                <p className="text-[12px]">Home</p>
+                <p className="text-[10px] font-medium">Home</p>
             </div>
-            <div className="hover:bg-gray-100 p-4 rounded-lg ml-1">
+            <div className="hover:bg-gray-100 px-1 text-center rounded-lg ml-1">
                 <img
                     className="w-9 h-10 mx-auto mt-2"
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwB-84DwBgGMVW3sRVPbh6Q1SGvKTpNNYd8A&s"
                     alt=""
                 />
+                <p className="text-[10px] font-medium">Shorts</p>
             </div>
-            <p className="text-[12px]">Shorts</p>
-            <div className="hover:bg-gray-100 p-4 rounded-lg ml-1">
+
+            <div className="hover:bg-gray-100 p-1 text-center rounded-lg ml-1">
                 <img
                     className="w-5 h-7 mx-auto mt-2"
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUF2WvlfBFamFtc_WwmyBsCHgP6WdvqAGqKw&s"
                     alt=""
                 />
+                <p className="text-[9px] font-medium">Subscriptions</p>
             </div>
-            <p className="text-[12px]">Subscriptions</p>
-            <div className="hover:bg-gray-100 p-4 rounded-lg ml-1">
+
+            <div className="hover:bg-gray-100 p-1 text-center rounded-lg ml-1">
                 <i className="fa-solid fa-user text-[23px] mx-2 mt-2"></i>
-                <p className="text-[12px]">You</p>
+                <p className="text-[10px] font-medium">You</p>
             </div>
         </div>
     );
